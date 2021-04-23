a97593fa5d4b2f2fea63251c2f03e4bec7de2bc8
Joshua M Cubas <joshuacubas@yahoo.com>
2021-04-23 13:58:39 -0500

    changed id for each todo task obj in todosArr to be Date.now() of creation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React, {useState} from 'react';
 import TodoForm from './components/TodoForm'
 
 function App() {
-  const [state,setState] = useState([])
+  const [todosArr,setTodosArr] = useState([])
 
   return (
     <div className="App">
       <header><h1>Things To Do List</h1></header>
       <br />
-      <TodoForm setStateArr={setState}/>
+      <TodoForm setTodosArr={setTodosArr} todosArr={todosArr}/>
     </div>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,14 +3,19 @@ import React, {useState} from 'react'
 export default function TodoForm(props) {
     const [input,setInput] = useState("");
 
+    const addTodo = () => {
+        const timeStr = Date.now();
+        props.setTodosArr([...props.todosArr,{text : input, id : parseInt(timeStr)}]);
+    }
+
     const handleChange = e => {
         setInput(e.target.value)
     };
 
     const handleSubmit = e => {
-        console.log("submission")
-        //stop the refresh on submission
         e.preventDefault();
+        addTodo();
+        setInput("");
     }
 
     return (
@@ -18,6 +23,7 @@ export default function TodoForm(props) {
             <form className="task-form" onSubmit={handleSubmit}>
                 <input
                     type="text"
+                    value={input}
                     placeholder="Description Here"
                     name="description"
                     className="form-input-box"
