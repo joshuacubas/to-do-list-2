75c09f1849ca9e8c17ea3ab8aaa87ce500e17e25
Joshua M Cubas <joshuacubas@yahoo.com>
2021-04-26 00:55:50 -0500

    delete button works, task dissappears from list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,14 @@ import React from 'react'
 import TodoTask from "./TodoTask"
 
 export default function todoList(props) {
-    const todoLis = props.todosArr.map(task => <TodoTask key={task.id} task={task}/>)
+    const todoLis = props.todosArr.map(task => 
+        <TodoTask 
+            key={task.id} 
+            task={task} 
+            setTodosArr={props.setTodosArr} 
+            todosArr={props.todosArr}
+        />
+    );
     
     return (
         <div>
--- src/components/TodoTask.js
+++ src/components/TodoTask.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 export default function TodoTask(props) {
+    const deleteTodoTask = () => {
+        console.log("delete task called",props.task.id)
+        let subtractedArr = props.todosArr.filter(todo => todo.id !== props.task.id);
+        props.setTodosArr(subtractedArr);
+
+    }
+
     return (
         <li>
             <div className="todo-text"><p>{props.task.text}</p></div>
             <div className="todo-buttons-div">
-                <button className="todo-buttons">✘</button>
+                <button
+                    onClick={deleteTodoTask}
+                    className="todo-buttons"
+                >✘
+                </button>
                 <button className="todo-buttons">✎</button>
             </div>
         </li>
